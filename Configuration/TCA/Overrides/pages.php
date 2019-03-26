a3ff48f92260da2c6fdad1be0f3cc386da0035a9
root <root@web9.internetgalerie.ch>
2019-03-26 15:51:50 +0100

    Bugfix: Wrong adding of fields to palettes (thanks to Ephraim Härer)

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,30 +1,42 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-  die ('Access denied.');
+    die ('Access denied.');
 }
 
 call_user_func(function () {
     // Configure new fields:
     $fields = array(
-		    'slug_locked' => array(
-					      'label' => 'LLL:EXT:ig_slug/Resources/Private/Language/locallang_db.xlf:tx_domain_model_page.slug_locked',
-					      'exclude' => 1,
-					      'config' => array(
-								'type' => 'check',
-								'renderType' => 'checkboxToggle',
-								'items' => [
-									    [
-									     0 => '',
-									     1 => '',
-									     ]
-									    ],
-								),
-					      )
-		    );
-    
+        'slug_locked' => array(
+            'label' => 'LLL:EXT:ig_slug/Resources/Private/Language/locallang_db.xlf:tx_domain_model_page.slug_locked',
+            'exclude' => 0,
+            'config' => array(
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        0 => '',
+                        1 => '',
+                    ]
+                ],
+            ),
+        )
+    );
+
     // Add new fields to pages:
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $fields);
     $GLOBALS['TCA']['pages']['columns']['slug']['config']['size'] = 100;
-    $GLOBALS['TCA']['pages']['palettes']['title']['showitem'] = 'title;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.title_formlabel, --linebreak--, slug, slug_locked, --linebreak--, nav_title;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.nav_title_formlabel, --linebreak--, subtitle;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.subtitle_formlabel';
-    $GLOBALS['TCA']['pages']['palettes']['titleonly']['showitem'] = 'title;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.title_formlabel, --linebreak--, slug, slug_locked';
-  });
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+        'pages',
+        'title',
+        'slug_locked',
+        'after:slug'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+        'pages',
+        'titleonly',
+        'slug_locked',
+        'after:slug'
+    );
+});
