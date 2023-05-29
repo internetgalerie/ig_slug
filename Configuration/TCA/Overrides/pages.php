c8f1217894b52e758f60cfbc1a972794be6addac
Peter Neumann <mail@peter-neumann.dev>
2023-05-29 13:15:36 +0200

    [TASK] Migrate items definition of slug_locked to new syntax
    * Refactor TCA to array short syntax and import ExtensionManagementUtility with use operator
    
    Resolves: #35

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,40 +1,42 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3') or die();
 
-call_user_func(function () {
-    // Configure new fields:
-    $fields = array(
-        'slug_locked' => array(
-            'label' => 'LLL:EXT:ig_slug/Resources/Private/Language/locallang_db.xlf:tx_domain_model_page.slug_locked',
-            'exclude' => 0,
-            'config' => array(
-                'type' => 'check',
-                'renderType' => 'checkboxToggle',
-                'items' => [
-                    [
-                        0 => '',
-                        1 => '',
-                    ]
-                ],
-            ),
-        )
-    );
+// Configure new fields:
+$fields = [
+    'slug_locked' => [
+        'label' => 'LLL:EXT:ig_slug/Resources/Private/Language/locallang_db.xlf:tx_domain_model_page.slug_locked',
+        'exclude' => 0,
+        'config' => [
+            'type' => 'check',
+            'renderType' => 'checkboxToggle',
+            'items' => [
+                [
+                    'label' => '',
+                    'labelChecked' => '',
+                    'labelUnchecked' => '',
+                ]
+            ],
+        ],
+    ]
+];
 
-    // Add new fields to pages:
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $fields);
-    $GLOBALS['TCA']['pages']['columns']['slug']['config']['size'] = 100;
+// Add new fields to pages:
+ExtensionManagementUtility::addTCAcolumns('pages', $fields);
+$GLOBALS['TCA']['pages']['columns']['slug']['config']['size'] = 100;
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
-        'pages',
-        'title',
-        '--linebreak--,slug_locked',
-        'after:slug'
-    );
+ExtensionManagementUtility::addFieldsToPalette(
+    'pages',
+    'title',
+    '--linebreak--,slug_locked',
+    'after:slug'
+);
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
-        'pages',
-        'titleonly',
-        '--linebreak--,slug_locked',
-        'after:slug'
-    );
-});
+ExtensionManagementUtility::addFieldsToPalette(
+    'pages',
+    'titleonly',
+    '--linebreak--,slug_locked',
+    'after:slug'
+);
