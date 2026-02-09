1c7aca9073b070f1c2dd52753374b92ab6ffd6f6
Daniel Abplanalp <da@internetgalerie.ch>
2026-02-09 13:14:18 +0100

    change module icon for v14

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,7 +3,11 @@
 return [
     'ig-slug-rebuild' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:ig_slug/Resources/Public/Icons/user_mod_rebuild.svg',
+        'source' => 'EXT:ig_slug/Resources/Public/Icons/' .
+            (
+                (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() < 14
+            ? 'module-ig-slug-v13.svg' : 'module-ig-slug.svg'
+            ),
     ],
 ];
 
